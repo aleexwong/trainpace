500c87141297857ae9d254376ad52d96b26d94c0
Alex Wong <wongalexjx@gmail.com>
2024-12-08 12:59:05 -0800

    add pwa to vite project and added netlify identity widget

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
