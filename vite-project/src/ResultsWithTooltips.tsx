38e26f3abc0bcbadd6053522b39d7be6216d64d1
Alex Wong <wongalexjx@gmail.com>
2024-12-10 00:01:45 -0800

    fixing capitalization issue and unused code

--- vite-project/src/ResultsWithTooltips.tsx
+++ vite-project/src/ResultsWithTooltips.tsx
@@ -105,7 +105,11 @@ const ResultsWithTooltips: React.FC<ResultsProps> = ({ results }) => {
                     >
                       <DialogContent>
                         <DialogHeader>
-                          <DialogTitle>{displayName} Pace</DialogTitle>
+                          <DialogTitle>
+                            {displayName.charAt(0).toUpperCase() +
+                              displayName.slice(1)}{" "}
+                            Pace
+                          </DialogTitle>
                         </DialogHeader>
                         <DialogDescription>
                           {paceTooltips[tooltipKey as PaceType]}
