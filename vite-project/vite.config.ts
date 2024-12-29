e6a607f3af85ba48e410d41a4b982f616a3a872f
Alex Wong <wongalexjx@gmail.com>
2024-12-29 14:02:10 -0800

    favicon added

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -17,18 +17,18 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000000',
-        icons: [
-          {
-            src: '/icons/trainPaceIcon.png', // Ensure these paths exist in your public folder
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/trainPaceIcon.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+        // icons: [
+        //   {
+        //     src: '/icons/trainPaceIcon.png', // Ensure these paths exist in your public folder
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: '/icons/trainPaceIcon.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        // ],
       },
     }),
   ],
