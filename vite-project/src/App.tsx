32f23b65a152f48eb3ce3918c00ce67bc561c1e2
Alex Wong <wongalexjx@gmail.com>
2025-04-20 13:45:22 -0700

    fuel planner

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { HelmetProvider } from "react-helmet-async";
 import Footer from "./Footer";
+import { Routes, Route, Link } from "react-router-dom";
 import TrainingPaceCalculator from "./TrainingPaceCalculator";
+import FuelPlanner from "./FuelPlanner";
 import { Toaster } from "@/components/ui/toaster";
 import GoogleAnalytics from "./lib/GoogleAnalytics";
 import { Button } from "./components/ui/button";
@@ -13,10 +15,17 @@ function App() {
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<TrainingPaceCalculator />} />
+        <Route path="/fuel" element={<FuelPlanner />} />
+      </Routes>
       <HelmetProvider>
         <GoogleAnalytics />
       </HelmetProvider>
-      <TrainingPaceCalculator />
+      {/* <TrainingPaceCalculator /> */}
+      <Link to="/fuel" className="text-blue-500 underline">
+        Go to Fuel Planner
+      </Link>
       <Button
         onClick={() => setShowPredictor(true)}
         className="fixed bottom-4 right-4 rounded-full p-3 bg-blue-600 text-white shadow-md z-50"
